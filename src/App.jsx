9acb5e5ca2a98b22027558e3602774e445a1394a
Flávia Reis <flaviarster@gmail.com>
2023-11-22 13:14:44 -0300

    Criei o primeiro componente e adicionei estilos

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ export function App() {
 
   return (
     <div className='App'>
-      <h1>WHAT'S THE MOVIE?</h1>
       < StartScreen />
     </div>
   )
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -2,6 +2,10 @@ import './StartScreen.css'
 
 export const StartScreen = () => {
   return (
-    <div>StartScreen</div>
+    <div className='start'>
+        <h1>WHAT'S THE MOVIE?</h1>
+        <p>Click the button below to start playing!</p>
+        <button>Let's Start</button>
+    </div>
   )
 }
