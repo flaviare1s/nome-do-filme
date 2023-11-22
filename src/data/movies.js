3c0d67efb56f8d50fef503a5767d193602133a96
Flávia Reis <flaviarster@gmail.com>
2023-11-22 11:50:18 -0300

    Instalei os arquivos e fiz as configurações iniciais

---
+++ src/data/movies.js
@@ -0,0 +1,6 @@
+export const wordsList = {
+  Sauron: ["The Lord Of The Rings"],
+  Expensive milk shake: ["Pulp Fiction"],
+  Ethan Hunt: ["Mission: Impossible"],
+  deadly phone call: ["Scream"],
+};
