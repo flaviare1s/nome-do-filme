42f15ea126c5e660f6752e39478a7a61b42f15e7
Flávia Reis <flaviarster@gmail.com>
2023-11-22 12:13:04 -0300

    Atualizei lista de filmes

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,6 +1,16 @@
-export const wordsList = {
-  Sauron: ["The Lord Of The Rings"],
-  Expensive milk shake: ["Pulp Fiction"],
-  Ethan Hunt: ["Mission: Impossible"],
-  deadly phone call: ["Scream"],
-};
+export const moviesList = {
+
+}
+
+/*
+  Sauron: The Lord Of The Rings,
+  Expensive milk shake: Pulp Fiction,
+  Ethan Hunt: Mission: Impossible,
+  Deadly phone call: Scream,
+  Talk to the hand: Terminator 3,
+  Wrath makes you lose your head: Seven,
+  I'm listen to the mockingjays: Hunger Games,
+  A puppet clown riding a bicycle 🤔: Jigsaw,
+  Books falling from the shelf: Interstellar,
+  A cat named Jinx: Meet the Parents
+  */
