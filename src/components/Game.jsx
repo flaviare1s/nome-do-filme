49765b05adf47d3a9455127b84ba40a2dc389481
Flávia Reis <flaviarster@gmail.com>
2023-11-22 22:43:41 -0300

    Correções no código do game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,164 +5,174 @@ import { useState , useEffect} from 'react'
 import { GameOver } from './GameOver'
 
 export function Game({ moviesData }) {
-  const [currentMovieIndex, setCurrentMovieIndex] = useState(0)
-  const [currentHintIndex, setCurrentHintIndex] = useState(0)
-  const [score, setScore] = useState(0)
-  const [lives, setLives] = useState(10)
-  const [guess, setGuess] = useState('')
-  const [congratulationsMessage, setCongratulationsMessage] = useState('')
-  const [lossMessage, setLossMessage] = useState('')
-  const [showOptions, setShowOptions] = useState(false)
-  const [hasGuessed, setHasGuessed] = useState(false)
-  const [showGuessBox, setShowGuessBox] = useState(true)
-
-  const currentMovie = moviesData[currentMovieIndex]
-  const totalHints = currentMovie.hints.length;
-  const remainingHints = totalHints - currentHintIndex - 1
-
-  const handleGuess = (userGuess) => {
-    console.log('Handling guess:', userGuess)
-    if (!hasGuessed) {
-      if (!userGuess.trim()) {
-        alert('Please make a guess!')
-        return
-      }
-
-      if (userGuess.toLowerCase() === currentMovie.title.toLowerCase()) {
-        const pointsEarned = 500 - currentHintIndex * 100
-        setScore(score + pointsEarned)
-
-        if (currentMovieIndex < moviesData.length - 1) {
-          alert('Congratulations! You got the movie right!')
-          setTimeout(() => {
-            setCurrentMovieIndex(currentMovieIndex + 1)
-            setCurrentHintIndex(0)
-            setHasGuessed(false)
-            setShowGuessBox(true)
-            setGuess('')
-          }, 500)
-        } else {
-          setCongratulationsMessage('You have completed the game!')
-          setShowOptions(true)
+    const [currentMovieIndex, setCurrentMovieIndex] = useState(0)
+    const [currentHintIndex, setCurrentHintIndex] = useState(0)
+    const [score, setScore] = useState(0)
+    const [lives, setLives] = useState(10)
+    const [guess, setGuess] = useState('')
+    const [congratulationsMessage, setCongratulationsMessage] = useState('')
+    const [lossMessage, setLossMessage] = useState('')
+    const [showOptions, setShowOptions] = useState(false)
+    const [hasGuessed, setHasGuessed] = useState(false)
+    const [showGuessBox, setShowGuessBox] = useState(true)
+    const [showHints, setShowHints] = useState(true)
+  
+    const currentMovie = moviesData[currentMovieIndex]
+    const totalHints = currentMovie.hints.length
+    const remainingHints = totalHints - currentHintIndex - 1
+  
+    const handleGuess = (userGuess) => {
+      console.log('Handling guess:', userGuess)
+      if (!hasGuessed) {
+        if (!userGuess.trim()) {
+          alert('Please make a guess!')
+          return
         }
-      } else {
-        setLives(lives - 1)
-
-        if (lives === 1) {
-          alert('You lost all your lives!')
-          setShowOptions(true)
+  
+        if (userGuess.toLowerCase() === currentMovie.title.toLowerCase()) {
+          const pointsEarned = 500 - currentHintIndex * 100
+          setScore(score + pointsEarned)
+  
+          if (currentMovieIndex < moviesData.length - 1) {
+            alert('Congratulations! You got the movie right!')
+            setTimeout(() => {
+              setCurrentMovieIndex(currentMovieIndex + 1)
+              setCurrentHintIndex(0)
+              setHasGuessed(false)
+              setShowGuessBox(true)
+              setShowHints(true)
+              setGuess('')
+            }, 500)
+          } else {
+            setCongratulationsMessage('You have completed the game!')
+            setShowOptions(true)
+          }
         } else {
-          alert('You lost a life!')
-          setShowOptions(true)
+          setLives(lives - 1)
+  
+          if (lives === 1) {
+            alert('You lost all your lives!')
+            setShowOptions(true)
+          } else {
+            alert('You lost a life!')
+            setShowOptions(true)
+          }
+  
+          setGuess('')
+          setHasGuessed(true)
+          setShowGuessBox(false)
+          setShowHints(false)
         }
-
-        setGuess('');
-        setHasGuessed(true)
-        setShowGuessBox(false)
       }
     }
-  }
-
-  const handleOptionClick = (option) => {
-    console.log('Option clicked:', option)
-
-    setShowOptions(false)
-
-    if (option === 'retry') {
-      setShowGuessBox(true)
-      setCurrentHintIndex(currentHintIndex + 1)
-      setHasGuessed(false)
-    } else if (option === 'changeMovie') {
-      setCurrentMovieIndex(currentMovieIndex + 1)
-
-      if (currentMovieIndex + 1 < moviesData.length) {
+  
+    const handleOptionClick = (option) => {
+      console.log('Option clicked:', option)
+  
+      setShowOptions(false)
+  
+      if (option === 'retry') {
         setShowGuessBox(true)
-        setCurrentHintIndex(0)
+        setCurrentHintIndex(currentHintIndex + 1)
         setHasGuessed(false)
-      } else {
-        setCongratulationsMessage('Congratulations! You finished the game!')
+        setShowHints(true)
+      } else if (option === 'changeMovie') {
+        setCurrentMovieIndex(currentMovieIndex + 1)
+  
+        if (currentMovieIndex + 1 < moviesData.length) {
+          setShowGuessBox(true)
+          setCurrentHintIndex(0)
+          setHasGuessed(false)
+          setShowHints(true)
+        } else {
+          setCongratulationsMessage('Congratulations! You finished the game!')
+          setShowOptions(true)
+        }
+      }
+    }
+  
+    const handleRestart = () => {
+      setCurrentMovieIndex(0)
+      setCurrentHintIndex(0)
+      setScore(0)
+      setLives(10)
+      setGuess('')
+      setCongratulationsMessage('')
+      setLossMessage('')
+      setShowOptions(false)
+      setHasGuessed(false)
+      setShowGuessBox(true)
+      setShowHints(true)
+    }
+  
+    const handleGuessChange = (event) => {
+      setGuess(event.target.value)
+    }
+  
+    const handleGuessSubmit = (event) => {
+      event.preventDefault()
+      handleGuess(guess)
+    }
+  
+    useEffect(() => {
+      console.log('Effect triggered. Lives:', lives)
+  
+      if (lives === 0 && !congratulationsMessage) {
+        setLossMessage('Game Over!')
         setShowOptions(true)
       }
-    }
-  };
-
-  const handleRestart = () => {
-    setCurrentMovieIndex(0)
-    setCurrentHintIndex(0)
-    setScore(0)
-    setLives(10)
-    setGuess('')
-    setCongratulationsMessage('')
-    setLossMessage('')
-    setShowOptions(false)
-    setHasGuessed(false)
-    setShowGuessBox(true)
-  };
-
-  const handleGuessChange = (event) => {
-    setGuess(event.target.value)
-  };
-
-  const handleGuessSubmit = (event) => {
-    event.preventDefault()
-    handleGuess(guess)
-  };
-
-  useEffect(() => {
-    console.log('Effect triggered. Lives:', lives)
-
-    if (lives === 0 && !congratulationsMessage) {
-      setLossMessage('Game Over!')
-      setShowOptions(true)
-    }
-  }, [lives, congratulationsMessage])
-
-  return (
-    <div>
-      {lives > 0 ? (
-        <>
-          <h1>Score: {score}</h1>
-          <h2>Lives: {lives}</h2>
-          <p>Hint: {currentMovie.hints[currentHintIndex]}</p>
-          <p>Remaining Hints: {Math.max(remainingHints, 0)}</p>
-
-          {congratulationsMessage && <p>{congratulationsMessage}</p>}
-          {lossMessage && <p>{lossMessage}</p>}
-
-          {showGuessBox && !showOptions && (
-            <form onSubmit={handleGuessSubmit}>
-              <label>
-                Guess the Movie:
-                <input type="text" value={guess} onChange={handleGuessChange} />
-              </label>
-              <p>
-                  <button type="submit">
-                    Take a guess
-                  </button>
-              </p>
-            </form>
-          )}
-
-          {showOptions && (
-            <div>
-              <p>Do you want another tip or do you want to change movie?</p>
-              <p><button onClick={() => handleOptionClick('retry')}>Keep trying</button></p>
-              <p><button onClick={() => handleOptionClick('changeMovie')}>Change movie</button></p>
-            </div>
-          )}
-
-          <div>
-            <h3>Hints:</h3>
-            <ul className='hints_box'>
-              {currentMovie.hints.slice(0, currentHintIndex + 1).map((hint, index) => (
-                <li key={index}>{hint}</li>
-              ))}
-            </ul>
-          </div>
-        </>
-      ) : (
-        <GameOver onRestart={handleRestart} />
-      )}
-    </div>
-  );
-}
+    }, [lives, congratulationsMessage])
+  
+    return (
+      <div>
+        {lives > 0 ? (
+          <>
+            <h1>Score: {score}</h1>
+            <h2>Lives: {lives}</h2>
+            <p>Hint: {currentMovie.hints[currentHintIndex]}</p>
+            <p>Remaining Hints: {Math.max(remainingHints, 0)}</p>
+  
+            {congratulationsMessage && <p>{congratulationsMessage}</p>}
+            {lossMessage && <p>{lossMessage}</p>}
+  
+            {showGuessBox && !showOptions && (
+              <form onSubmit={handleGuessSubmit}>
+                <label>
+                  Guess the Movie:
+                  <input type="text" value={guess} onChange={handleGuessChange} />
+                </label>
+                <p>
+                  <button type="submit">Take a guess</button>
+                </p>
+              </form>
+            )}
+  
+            {showOptions && (
+              <div>
+                <p>Do you want another tip or do you want to change movie?</p>
+                <p>
+                  <button onClick={() => handleOptionClick('retry')}>Keep trying</button>
+                </p>
+                <p>
+                  <button onClick={() => handleOptionClick('changeMovie')}>Change movie</button>
+                </p>
+              </div>
+            )}
+  
+            {showHints && (
+              <div>
+                <h3>Hints:</h3>
+                <ul className='hints_box'>
+                  {currentMovie.hints.slice(0, currentHintIndex + 1).map((hint, index) => (
+                    <li key={index}>{hint}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </>
+        ) : (
+          <GameOver onRestart={handleRestart} />
+        )}
+      </div>
+    )
+  }
